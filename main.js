9c91917b11582eb8daabf233ed607292409b0e6d
pedrofolador <106595220+pedrofolador@users.noreply.github.com>
2024-06-13 19:14:56 -0300

    hypersave

---
+++ main.js
@@ -0,0 +1,22 @@
+const caixaPrincipal = document.querySelector(caixaprincipal);
+const caixaPerguntas = document.querySelector(caixaperguntas);
+const caixaAlternativas = document.querySelector(caixaalternativas);
+const caixaResultado = document.querySelector(caixaresultado);
+const textoResultado = document.querySelector(textoresultado);
+
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
