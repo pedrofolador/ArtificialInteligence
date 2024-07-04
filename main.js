bd685dee5c01f655abc778dd263c39f5c882d619
pedrofolador <106595220+pedrofolador@users.noreply.github.com>
2024-07-04 19:10:48 -0300

    time for Jack to LET IT RIP

--- main.js
+++ main.js
@@ -117,3 +117,16 @@ function mostraResultado() {
 }
 
 mostraPergunta();
+
+function aleatorio(lista) {
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+        const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+    historiaFinal += afirmacoes + “ “;
+    atual++;
+    mostraPergunta();
+}
+    
